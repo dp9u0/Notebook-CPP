4e1be2d6c975dd10dfbb5d42e7d7823d2763998a
guodp <guodp9u0@gmail.com>
2021-06-28 09:29:37 +0800

    update ch01

---
+++ src/ch01/1.1.cpp
@@ -0,0 +1,5 @@
+// g++ --std=c++11 1.1.cpp -o 1.1.out
+int main()
+{
+	return 0;
+}
---
+++ src/ch01/1.2.cpp
@@ -0,0 +1,7 @@
+// g++ --std=c++11 1.2.cpp -o 1.2.out
+// ./1.2.out
+// echo $? # 输出 255 https://tldp.org/LDP/abs/html/exitcodes.html
+int main()
+{
+	return -1;
+}
