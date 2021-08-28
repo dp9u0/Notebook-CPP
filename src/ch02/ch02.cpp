a295e7aa8d219d1d3b6672acad3beb0432058900
guodp <guodp9u0@gmail.com>
2021-08-28 16:16:43 +0800

    update ch02 : 类型转换

--- src/ch02/ch02.cpp
+++ src/ch02/ch02.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-void sizeoftypes()
+void size_of_types()
 {
   std::cout << "size of 'bool' : " << sizeof(bool) << std::endl;
   std::cout << "size of 'char' : " << sizeof(char) << std::endl;
@@ -14,8 +14,22 @@ void sizeoftypes()
   std::cout << "max of 'int 32' : " << INT32_MAX << std::endl;
   std::cout << "max of 'uint 32' : " << UINT32_MAX << std::endl;
 }
+
+void int_to_float()
+{
+
+  // 赋值给 float 的整型 超出 float 范围 会导致精度损失
+  long long ll1 = 3828375778387558400;
+  float f = ll1;
+  long long ll2 = f;
+  std::cout << "ll1 : " << ll1 << std::endl;
+  std::cout << "ll2 : " << ll2 << std::endl;
+}
+
 int main()
-{ 
-  sizeoftypes();
+{
+  size_of_types();
+
+  int_to_float();
   return 0;
 }
