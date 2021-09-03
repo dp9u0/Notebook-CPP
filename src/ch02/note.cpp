d5e9aa0662e989d5757ee02a81362bd6c0c98dab
guodp <guodp9u0@gmail.com>
2021-09-03 13:26:34 +0800

    update ch02 : constexpr

--- src/ch02/note.cpp
+++ src/ch02/note.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Sales_item.h"
+using namespace std;
 
 void size_of_types()
 {
@@ -151,7 +152,40 @@ void const_pointer()
   int i = 100;
   int const *pi = &i;
   const int ci = 100;
-  const int const *pci = &ci;
+  const int *const pci = &ci;
+}
+
+//普通函数的声明
+int noconst_dis(int x);
+//常量表达式函数的声明
+constexpr int display(int x);
+
+void const_expr()
+{
+  //调用常量表达式函数
+  int a[display(3)] = {1, 2, 3, 4};
+  cout << a[2] << endl;
+  //调用普通函数
+  cout << noconst_dis(3) << endl;
+}
+
+//常量表达式函数的定义
+constexpr int display(int x)
+{
+  return 1 + 2 + x;
+}
+
+//普通函数的定义
+int noconst_dis(int x)
+{
+  return 1 + 2 + x;
+}
+
+void const_expr_pointer()
+{
+  const int *p = nullptr;
+  constexpr int *q = nullptr;
+  constexpr const int *q1 = nullptr;
 }
 
 int main()
