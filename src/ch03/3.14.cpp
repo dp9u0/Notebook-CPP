e5d15a5b1a147c36b5468c609332aefc6bb3b95b
guodp <guodp9u0@gmail.com>
2021-09-05 20:50:18 +0800

    update ch03 : vector

---
+++ src/ch03/3.14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  vector<int> vi;
+  int input;
+  while (cin >> input)
+  {
+    vi.push_back(input);
+  }
+  return 0;
+}
--- src/ch03/note.cpp
+++ src/ch03/note.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 void string_io()
@@ -7,7 +9,6 @@ void string_io()
   string s;
   cin >> s; // 将 string 对象读取到s 遇到空白停止
   cout << s << endl;
-
   string s1, s2;
   cin >> s1 >> s2; // 将 string 对象读取到s 遇到空白停止
   cout << s1 << endl;
@@ -26,8 +27,40 @@ void string_op()
   cout << s << endl;
 }
 
+void vector_init()
+{
+  cout << " ---------- vector_init ---------- " << endl;
+  vector<int> v1;
+  vector<int> v2(v1);
+  vector<int> v3 = v2; // 拷贝
+  vector<int> v4(10, 100);
+  vector<int> v5{1, 2, 3, 4, 5};
+  vector<int> v6 = {1, 2, 3, 4, 5};
+  v2.push_back(100);
+
+  cout << "v2.size():" << v2.size() << endl;
+  cout << "v3.size():" << v3.size() << endl;
+}
+
+void vector_op()
+{
+  cout << " ---------- vector_op ---------- " << endl;
+  vector<int> v1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  for (auto &i : v1) // 这里是引用
+  {
+    i = i * i;
+  }
+  for (auto i : v1)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+}
+
 int main()
 {
-  string_io();
-  string_op();
+  // string_io();
+  // string_op();
+  // vector_init();
+  vector_op();
 }
