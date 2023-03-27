15807c087715b5bbefeadd9be0fa178265d4b3ba
guodp <guodp9u0@gmail.com>
2023-03-27 13:26:29 +0800

    update ch05: struct union enum

--- intro_c/src/5.1.c
+++ intro_c/src/5.1.c
@@ -13,6 +13,7 @@ typedef struct
 {
   char *name;
   int age;
+  week dayOfWeek;
 } student;
 
 typedef union
@@ -20,3 +21,26 @@ typedef union
   int a;
   int b;
 } data;
+
+#include <stdio.h>
+
+void print1(const student *stu)
+{
+  printf("%s:%d,%d\n", stu->name, stu->age, stu->dayOfWeek);
+  // stu->age = 100; cannot modify
+}
+
+void print2(student *stu)
+{
+  printf("%s:%d,%d\n", stu->name, stu->age, stu->dayOfWeek);
+  stu->age = 30; // CAN modify
+}
+
+int main(int argc, char const *argv[])
+{
+  const student lihua = {"lihua", 18, Mon};
+  printf("%s:%d,%d\n", lihua.name, lihua.age, lihua.dayOfWeek);
+  print2(&lihua); // 只有警告 warning: passing argument 1 of ‘print2’ discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
+  print1(&lihua);
+  return 0;
+}
